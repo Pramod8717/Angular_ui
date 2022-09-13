53aaa0135af0115824f03fe28cfdfeaaf1ec5b26
Pramodshinde8717 <pramods.shinde@cloverinfotech.com>
2022-09-13 22:23:34 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyContainer } from './myContainer/myContainer.component';
+import { TopnavComponent } from './myContainer/topnav/topnav.component';
+import { HeaderComponent } from './myContainer/header/header.component';
+import { Txtsec1Component } from './myContainer/txtsec1/txtsec1.component';
+import { Txtsec2Component } from './myContainer/txtsec2/txtsec2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainer,
+    TopnavComponent,
+    HeaderComponent,
+    Txtsec1Component,
+    Txtsec2Component
+    
   ],
   imports: [
     BrowserModule,
